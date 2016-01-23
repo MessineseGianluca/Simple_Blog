f16a25a25c1afeb5a11f5d8e15c02fb0aebd3365
MessineseGianluca <gianluca.messi1@gmail.com>
2016-01-23 20:47:24 +0100

    Fix login, Embeed bootstrap into index.php

--- index.php
+++ index.php
@@ -6,25 +6,52 @@
   {
     header("Location: login.html");
     exit;
-  }
-  echo "Benvenuto " . $_SESSION['name']. ' ' . $_SESSION['surname'];
-  
+  }  
   // remove all session variables
   #session_unset(); 
   // destroy the session 
   #session_destroy(); 
 ?>
-<html>
-<head>
+<!DOCTYPE html>
+<html lang='en'>
   <title> Dashboard </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/mycss.css">
 </head>
 <body>
-  <header>
-    <h1> My Simple Blog </h1>
-  </header>
-  <div class='dashboard'>
+  <div class='container'>
+    <div class='page-header'>
+      <h1><b> My Simple Blog </b></h1>
+    </div>
     
+    <div class='row'>
+      <div class='col-lg-4 user-panel' >
+        <div class='row'>
+          <div class='col-lg-2'>
+            <img src='img/user.jpg' class='img-rounded user-img'>
+          </div>
+          
+          <div class='col-lg-8' >
+            <p><?php echo $_SESSION['name']. ' ' . $_SESSION['surname']; ?></p>
+          </div>
+          
+         <div class="btn-group col-lg-2">
+            <button type="button" class="btn btn-default dropdown-toggle" data-
+            toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="logout.php">Exit</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap/dist/js/bootsrap.min.js"></script>
 </body>
 </html> 
 
