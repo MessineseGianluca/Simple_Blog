ca2cc342f93c49b5fa66d443b1f2fa5d6f6f68a6
MessineseGianluca <gianluca.messi1@gmail.com>
2016-01-25 01:10:39 +0100

    Fix css code,Create new post panel, Fix logou

--- index.php
+++ index.php
@@ -28,6 +28,12 @@
     </div>
     
     <div class='row'>
+      <div class="col-lg-8">  
+        <!-- A panel who contains items for insert new post-->
+        <img class='adder-img' src='img/add.png' id='add'>
+        <img class='adder-img' src='img/add-picture.png'>
+      </div> 
+      
       <div class='col-lg-4 user-panel' >
         <div class='row'>
           <div class='col-lg-2'>
@@ -37,22 +43,39 @@
           <div class='col-lg-8' >
             <p><?php echo $_SESSION['name']. ' ' . $_SESSION['surname']; ?></p>
           </div>
-          
-          <div class="dropdown">
-            <button class="btn btn-default dropdown-toggle" type="button" 
-            data-toggle="dropdown">
-              <span class="caret"></span>
+
+          <div class='dropdown col-lg-2'>
+            <button class='btn btn-default dropdown-toggle' type='button' 
+            data-toggle='dropdown'>
+              <span class='caret'></span>
             </button>
-            <ul class="dropdown-menu" >
-              <li><a tabindex="-1" href="logout.php">Log out</a></li>
+            <ul class='dropdown-menu dropdown-menu-right' >
+              <li><a tabindex='-1' href='logout.php'>Log out</a></li>
             </ul>
           </div>
         </div>
       </div>
+      
     </div>
+    
+    <!-- A row for new posts -->
+    <div class='row'>
+      <div class="form-group col-lg-8" id='add-post'>
+        <form action='post.php' method='post'>
+          <label style='color:white'>New post:</label>
+          <textarea class='form-control' style='resize: none' 
+          rows='2' name='description'></textarea>
+          <input style='float:right; margin-top:5px;' 
+          class='btn btn-default' type='submit'>
+        </form>
+      </div>  
+    </div>
+    
+    
   </div>
   <script src="bower_components/jquery/dist/jquery.min.js"></script>
   <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="js/myjs.js"></script>
   </body>
 </html> 
 
---
+++ js/myjs.js
@@ -0,0 +1,4 @@
+$('#add').click(function() {
+  $('#add-post').css('visibility', 'visible');
+});
+
