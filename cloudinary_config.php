5732397c6b61264572d3e38b96eb18f6ddc22c4c
MessineseGianluca <gianluca.messi1@gmail.com>
2016-04-07 16:58:23 +0200

    Create Cloudinary Config

---
+++ cloudinary_config.php
@@ -0,0 +1,11 @@
+<?php 
+
+  function getCloudinaryCredentials() {
+    return array( 
+      "cloud_name" => "gianluca", 
+      "api_key" => "589937394867361", 
+      "api_secret" => "h9SigaBXJVvtu4Mf7Svyz_VQsKU" 
+  	);
+
+  }
+
--- validate_new_user.php
+++ validate_new_user.php
@@ -4,6 +4,7 @@
   require 'vendor/cloudinary/cloudinary_php/src/Uploader.php';
   require 'vendor/cloudinary/cloudinary_php/src/Api.php';  
   include 'php_functions.php';
+  include 'cloudinary_config.php';
   
   $name = $_POST['name'];
   $surname = $_POST['surname'];
@@ -41,21 +42,8 @@
       exit;
     }
 
-    /*save file specified in the first parameter into the 
-     *directory specified in the second parameter
-     */
-    //move_uploaded_file(
-      //$_FILES['imgToUpdate']['tmp_name'], 
-      //'img/' . $user_id 
-    //);
-
-
-    \Cloudinary::config(array( 
-      "cloud_name" => "gianluca", 
-      "api_key" => "589937394867361", 
-      "api_secret" => "h9SigaBXJVvtu4Mf7Svyz_VQsKU" 
-    ));
-
+    \Cloudinary::config(getCloudinaryCredentials());
+    
     $img_url = \Cloudinary\Uploader::upload(
         $_FILES['imgToUpdate']['tmp_name'],
         array(
@@ -66,6 +54,7 @@
     );
     
     $img_url = $img_url['url'];
+
   }
 
   /* IF EVERYTHING IS OKAY, CRIPT THE PASSWORD AND STORE THE NEW USER
